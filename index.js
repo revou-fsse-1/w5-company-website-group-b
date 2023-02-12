668fc23420a20fc8ae7e153dfec58ceb8083ee53
yankyhermawan <yankyhermawan42@gmail.com>
2023-02-12 15:11:11 +0700

    Init File

---
+++ index.js
@@ -0,0 +1,8 @@
+function myFunction() {
+	if (document.getElementById("toggle").checked) {
+		document.body.style.backgroundColor = "white";
+	}
+	if (document.getElementById("toggle").checked == false) {
+		document.body.style.backgroundColor = "black";
+	}
+}
